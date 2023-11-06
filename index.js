f53d979fe34bf1f8d757e14ab72ad2ab17ab647d
tushar-454 <imtushar454@gmail.com>
2023-11-06 14:52:10 +0600

    service-swap-center server code push and git check

---
+++ index.js
@@ -0,0 +1,40 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const port = process.env.port || 5000;
+// middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@service-swap-center.0rpazty.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Api is working' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running http://localhost:${port}`);
+});
