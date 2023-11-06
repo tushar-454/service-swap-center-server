dcd4655ca265c3057c192931287a79df6545f9be
tushar-454 <imtushar454@gmail.com>
2023-11-06 23:18:11 +0600

    update services by services id api done

--- index.js
+++ index.js
@@ -72,9 +72,35 @@ async function run() {
     // delete user added service by uid
     app.delete('/services/:id', async (req, res) => {
       const id = req.params.id;
-      const result = await bookingCollection.deleteOne({
+      const result = await servicesCollection.deleteOne({
         _id: new ObjectId(id),
       });
+      res.send(result);
+    });
+
+    // update service by service id
+    app.put('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const getupdatedDoc = req.body;
+      const updatedDoc = {
+        $set: {
+          email: getupdatedDoc.email,
+          authorImage: getupdatedDoc.authorImage,
+          authorName: getupdatedDoc.authorName,
+          name: getupdatedDoc.name,
+          image: getupdatedDoc.image,
+          price: getupdatedDoc.price,
+          servicearea: getupdatedDoc.servicearea,
+          description: getupdatedDoc.description,
+        },
+      };
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const result = await servicesCollection.updateOne(
+        query,
+        updatedDoc,
+        options
+      );
       res.send(result);
     });
 
