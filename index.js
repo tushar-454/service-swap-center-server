eeb4b9658f92fd52d16f44085986f99181da9946
tushar-454 <imtushar454@gmail.com>
2023-11-08 18:31:38 +0600

    cors origin issue fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: 'https://service-swap-center.web.app',
     credentials: true,
   })
 );
